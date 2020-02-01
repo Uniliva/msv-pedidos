a422763fdf861fa883e4a554f90fdaf2d01f5fe9
Uniliva Alves <uniliva@hotmail.com>
2020-01-31 23:16:39 -0300

    Criando estrutura do msv

---
+++ src/main/java/br/com/unicontas/pedidos/MsvPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.unicontas.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unicontas/pedidos/MsvPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unicontas.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
